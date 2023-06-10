3d95b89ffabd56e8dfc72d80fa39de2796530c03
softRahul10 <softrahul10@gmail.com>
2023-06-10 17:46:18 +0530

    Cart state managed by redux and action dispatching by compoents done

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -2,7 +2,7 @@ import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button onClick={props.onClick} className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,14 +1,27 @@
+import { useDispatch } from 'react-redux';
+
+import { cartShowActions } from '../../store/cartShowSlice';
+
 import CartButton from '../Cart/CartButton';
 import classes from './MainHeader.module.css';
 
+
+
 const MainHeader = (props) => {
+
+    const dispatch = useDispatch();
+
+    const toggleCartHandler = () => {
+        dispatch(cartShowActions.toggle());
+    }
+
     return (
         <header className={classes.header}>
             <h1>ReduxCart</h1>
             <nav>
                 <ul>
                     <li>
-                        <CartButton />
+                        <CartButton onClick={toggleCartHandler} />
                     </li>
                 </ul>
             </nav>
