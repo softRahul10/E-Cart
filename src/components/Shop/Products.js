dc219afd103bf842d2a91eb87d0aae280b2aed54
softRahul10 <softrahul10@gmail.com>
2023-06-10 17:54:42 +0530

    Sample data for products added

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,6 +1,33 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const fakeProducts = [
+    {
+        id:'p1',
+        title:'School Bag',
+        price: 25,
+        description:"This is just a dummy product description written for testing",
+    },
+    {
+        id:'p2',
+        title:'Smartphone',
+        price: 500,
+        description:"This is just a dummy product description written for testing",
+    },
+    {
+        id:'p3',
+        title:'Books',
+        price: 12,
+        description:"This is just a dummy product description written for testing",
+    },
+    {
+        id:'p4',
+        title:'Makeup Products',
+        price: 925,
+        description:"This is just a dummy product description written for testing",
+    }
+];
+
 const Products = (props) => {
     return (
         <section className={classes.products}>
